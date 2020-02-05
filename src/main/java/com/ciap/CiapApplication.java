4573558f266ada1faa3894bef58f4f2e6949827b
ypq <hrbypq@163.com>
2020-02-05 21:31:25 +0800

    Initialization

---
+++ src/main/java/com/ciap/CiapApplication.java
@@ -0,0 +1,11 @@
+package com.ciap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiapApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CiapApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ciap/CiapApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ciap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiapApplicationTests {
+	@Test
+	void contextLoads() {
+
+	}
+}
