6810e028855c54da394ff62bb08040caad7f9ba9
ypq <hrbypq@163.com>
2020-02-14 20:18:25 +0800

    Modified Service.

--- src/main/java/com/ciap/service/AccountService.java
+++ src/main/java/com/ciap/service/AccountService.java
@@ -1,5 +1,7 @@
 package com.ciap.service;
 
+import com.ciap.entity.Student;
+import com.ciap.entity.Teacher;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,5 +12,49 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
+    /**
+     * 创建学生用户
+     * @param a_student
+     * @return
+     */
+    public boolean createStudentAccount(Student a_student){
+        return true;
+    }
 
+    /**
+     *
+     * @param a_teacher
+     * @return
+     */
+    public boolean createTeacherAccount(Teacher a_teacher){
+        return true;
+    }
+
+    /**
+     *
+     * @param a_id
+     * @return
+     */
+    public boolean updateStudentPassword(String a_id){
+        return true;
+    }
+
+    /**
+     *
+     * @param a_id
+     * @return
+     */
+    public boolean updateTeacherPassword(String a_id){
+        return true;
+    }
+
+    /**
+     *
+     * @param a_id
+     * @param a_intro
+     * @return
+     */
+    public boolean updateTeacherInfo(String a_id,String a_intro){
+        return true;
+    }
 }
--- src/main/java/com/ciap/service/CommentService.java
+++ src/main/java/com/ciap/service/CommentService.java
@@ -1,6 +1,9 @@
 package com.ciap.service;
 
+import com.ciap.entity.Comment;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 /**
  * 评论服务类
@@ -10,5 +13,30 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
-    
+    /**
+     *
+     * @param a_comment
+     * @return
+     */
+    public boolean createComment(Comment a_comment){
+        return true;
+    }
+
+    /**
+     *
+     * @param a_curr_id 课程主键
+     * @return
+     */
+    public Set<Comment>printComment(String a_curr_id){
+        return null;
+    }
+
+    /**
+     *
+     * @param a_id
+     * @return
+     */
+    public boolean deleteComment(String a_id){
+        return true;
+    }
 }
