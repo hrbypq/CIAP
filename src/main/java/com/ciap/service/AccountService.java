dda149f91e3bca85d617c49e42fe8c619b1d2772
lhy <liheyang1999@163.com>
2020-02-24 22:14:24 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   src/main/java/com/ciap/service/AccountService.java

--- src/main/java/com/ciap/service/AccountService.java
+++ src/main/java/com/ciap/service/AccountService.java
@@ -5,6 +5,7 @@ import com.ciap.dao.StudentRepository;
 import com.ciap.dao.TeacherRepository;
 import com.ciap.entity.Student;
 import com.ciap.entity.Teacher;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +35,8 @@ public class AccountService {
      * @return
      */
     public boolean createStudentAccount(Student a_student){
+        if(a_student==null)
+            return false;
         if(!studentRepository.existsById(a_student.getId())) {
             Student res = null;
             res = studentRepository.save(a_student);
@@ -49,6 +52,8 @@ public class AccountService {
      * @return
      */
     public boolean createTeacherAccount(Teacher a_teacher){
+        if(a_teacher==null)
+            return false;
         if(!teacherRepository.existsById(a_teacher.getId())) {
             Teacher res = null;
             res = teacherRepository.save(a_teacher);
@@ -106,5 +111,36 @@ public class AccountService {
         else
             return false;
 
+    }
+    /**
+     * @param  a_id
+     * @return
+     */
+    public Teacher searchTeacher(String a_id){
+     Teacher teacher=null;
+     teacher=teacherRepository.findById(a_id).get();
+     return  teacher;
+    }
+    /**
+     * @param a_id
+     * @param a_pwd
+     * @return
+     */
+    public boolean signTeacher(String a_id,String a_pwd){
+        if(!teacherRepository.existsById(a_id))
+            return false;
+        Teacher teacher=searchTeacher(a_id);
+        return a_pwd.equals(teacher.getPassword());
+    }
+    /**
+     * @param a_id
+     * @param a_pwd
+     * @return
+     */
+    public boolean signStudent(String a_id,String a_pwd){
+        if(!studentRepository.existsById(a_id))
+            return false;
+        Student student=studentRepository.findById(a_id).get();
+        return a_pwd.equals(student.getPassword());
     }
 }
