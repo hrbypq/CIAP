3856415d90279ffb49ed5df46fffce77fc74a483
ypq <hrbypq@163.com>
2020-02-25 21:07:45 +0800

    Modified AccountService

--- src/main/java/com/ciap/service/AccountService.java
+++ src/main/java/com/ciap/service/AccountService.java
@@ -5,7 +5,6 @@ import com.ciap.dao.StudentRepository;
 import com.ciap.dao.TeacherRepository;
 import com.ciap.entity.Student;
 import com.ciap.entity.Teacher;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
